f6c916c6df13cc5ad3544e6eb2cf0815e7d3189a
Pavel Shakhov <ab.pongo@gmail.com>
2026-04-13 11:06:19 +0300

    feat(pastelColor): stripping leading '-' before hashing

--- src/utils/pastelColor.ts
+++ src/utils/pastelColor.ts
@@ -7,6 +7,7 @@ const colorHash = new ColorHash({ saturation: 0.7, lightness: 0.9 });
  * Generate a pastel color from a given string
  */
 export function generatePastelColor(text: string): string {
-  if (!text.trim()) return "#fff";
-  return colorHash.hex(firstLine(text));
+  text = firstLine(text).replace(/^-/, "");
+  if (!text) return "#fff";
+  return colorHash.hex(text);
 }
